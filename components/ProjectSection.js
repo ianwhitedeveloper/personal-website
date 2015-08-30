4dd887206151b42ab47ecb7a4155671bfeb4ab76
Ian White <ian.white@t-3.com>
2015-08-29 22:03:58 -0500

    add prop types check

--- components/ProjectSection.js
+++ components/ProjectSection.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import Project from '../components/Project';
 
 export default class ProjectSection extends Component {
@@ -15,4 +15,9 @@ export default class ProjectSection extends Component {
 			</section>
 		);
 	}
+}
+
+ProjectSection.propTypes = {
+	projects: PropTypes.array.isRequired,
+	actions: PropTypes.object.isRequired
 }
