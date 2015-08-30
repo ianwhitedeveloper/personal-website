e60e1b04448132e8463171c150a575d2205de4ff
Ian White <ian.white@t-3.com>
2015-08-29 19:47:50 -0500

    fix stuff

--- components/Project.js
+++ components/Project.js
@@ -6,13 +6,11 @@ export default class Project extends Component {
 		const { likeProject, project } = this.props;
 
 		return (
-			<ul>
-					return	<li>
-										<h1>Project title:{project.title}</h1>
-										<h2>Likes: {project.likes}</h2>
-										<button onClick={() => likeProject(project.id)}>Like!</button>
-									</li>
-			</ul>
+			<li>
+				<h1>Project title:{project.title}</h1>
+				<h2>Likes: {project.likes}</h2>
+				<button onClick={() => likeProject(project.id)}>Like!</button>
+			</li>
 		);
 	}
 }
