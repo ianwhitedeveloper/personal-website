b5f865b82d4b25ccc2d2926ae2bca57423afd491
Ian White <ian.white@t-3.com>
2015-09-02 15:01:04 -0500

    edit education modules

--- components/EducationSection.js
+++ components/EducationSection.js
@@ -4,14 +4,18 @@ export default class EducationSection {
 	render() {
 		return (
 			<section className='row'>
-				<div className='col-sm-6 mks'>
-					<h1>MakerSquare</h1>
-					<p>Completed 200+ hours of pre work prior to the course in order to get a solid foundation in basics of HTML5, CSS3, Ruby and Javascript. MakerSquare is a highly selective 3 month bootcamp style course teaching full-stack web development 60+ hours a week. - April 2014</p>
+				<div className='col-sm-6'>
+					<div className='mks'>
+						<h1>MakerSquare</h1>
+						<p>Completed 200+ hours of pre work prior to the course in order to get a solid foundation in basics of HTML5, CSS3, Ruby and Javascript. MakerSquare is a highly selective 3 month bootcamp style course teaching full-stack web development 60+ hours a week. - April 2014</p>
+					</div>
 				</div>
 
-				<div className='col-sm-6 ut'>
-					<h1>UT Austin</h1>
-					<p>Completed degree in Psychology as full time student and employee at Glazer’s Distributors where I was previously in merchandising and wine sales. - December 2009</p>
+				<div className='col-sm-6'>
+					<div className='ut'>
+						<h1>UT Austin</h1>
+						<p>Completed degree in Psychology as full time student and employee at Glazer’s Distributors where I was previously in merchandising and wine sales. - December 2009</p>
+					</div>
 				</div>
 			</section>
 		);
