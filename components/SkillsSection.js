7df501710cd8a0d7bc4eeb3491ca2fe1f70846b3
Ian White <ian.white@t-3.com>
2015-09-02 15:23:03 -0500

    add react animation to skills filtering

--- components/SkillsSection.js
+++ components/SkillsSection.js
@@ -1,19 +1,26 @@
 import React,  {PropTypes} from 'react';
 import TextInput from './TextInput'
 
+const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
 export default class SkillsSection {
 	render() {
 		const { skills, filterSkills, actions } = this.props;
 
+		let filteredSkills = skills.map((skill, i) => {
+					if (skill.show) {
+						return <div className='skill' style={{padding: '10px 10px', margin: '10px auto', float: 'left'}} key={i}>{skill.title}</div>
+					}
+				}.bind(this));
+
 		return (
+
 			<section className='row'>
 				<TextInput {...actions} placeholder='Search...' />
 				<div style={{textAlign: 'center'}}>
-					{skills.map((skill, i) => {
-						if (skill.show) {
-							return <div className='skill' style={{padding: '10px 10px', margin: '10px auto', float: 'left'}} key={i}>{skill.title}</div>
-						}
-					})}
+					<ReactCSSTransitionGroup transitionName="example">
+	          {filteredSkills}
+	        </ReactCSSTransitionGroup>
 				</div>
 			</section>
 		);
