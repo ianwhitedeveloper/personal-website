d9311f9eb57ab2085de0f2b3a90f99a8de58a96f
Ian White <ian.white@t-3.com>
2015-09-02 13:43:55 -0500

    add ut and mks images

--- components/EducationSection.js
+++ components/EducationSection.js
@@ -3,12 +3,12 @@ import React from 'react';
 export default class EducationSection {
 	render() {
 		return (
-			<section>
-				<div>
+			<section className='row'>
+				<div className='col-sm-6 mks'>
 					<h1>MakerSquare</h1>
 				</div>
 
-				<div>
+				<div className='col-sm-6 ut'>
 					<h1>UT</h1>
 				</div>
 			</section>
