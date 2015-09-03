04968ded4f197c778bb4ff59ef7f2ca9466d7247
Ian White <ian.white@t-3.com>
2015-09-02 23:05:26 -0500

    cool flexbox shit with skills!

--- components/SkillsSection.js
+++ components/SkillsSection.js
@@ -9,7 +9,7 @@ export default class SkillsSection {
 
 		let filteredSkills = skills.map((skill, i) => {
 					if (skill.show) {
-						return <div className='skill' style={{padding: '10px 10px', margin: '10px auto', float: 'left'}} key={i}>{skill.title}</div>
+						return <div className='skill' style={{padding: '10px 10px', margin: '10px auto'}} key={i}>{skill.title}</div>
 					}
 				}.bind(this));
 
@@ -18,8 +18,8 @@ export default class SkillsSection {
 			<section style={{backgroundColor: '#fff', padding: '0 15px', boxShadow: '0 14px 45px rgba(0, 0, 0, 0.25), 0 10px 18px rgba(0, 0, 0, 0.22)'}}>
 				<div className='row'>
 					<TextInput {...actions} placeholder='Search...' />
-					<div style={{textAlign: 'center'}}>
-						<ReactCSSTransitionGroup transitionName="example">
+					<div>
+						<ReactCSSTransitionGroup style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', textAlign: 'center'}} transitionName="example">
 		          {filteredSkills}
 		        </ReactCSSTransitionGroup>
 					</div>
