aace1d643605eb85fb0a1e82d6b4b8c0a96c58af
Ian White <ian.white@t-3.com>
2015-09-03 11:14:04 -0500

    add a couple of css animations

--- components/Header.js
+++ components/Header.js
@@ -18,9 +18,9 @@ export default class Header {
 	render() {
 		return (
 			<header style={{display: 'flex', flexFlow: 'row no-wrap', justifyContent: 'space-between', padding: '15px 0'}}>
-				<figure style={logoStyles}>
+				<figure className='animated flipInY' style={logoStyles}>
 				</figure>
-				<figcaption style={{textAlign: 'right', maxWidth: '50%', alignSelf: 'center', fontFamily: '"Amatic SC", cursive'}}>
+				<figcaption className='animated flipInX' style={{textAlign: 'right', maxWidth: '50%', alignSelf: 'center', fontFamily: '"Amatic SC", cursive'}}>
 					<p>Hi. My name is Ian White and I'm a full stack web developer based in <span style={{color: '#B0BEC5', fontSize: '1.2em', textShadow: '1px 1px 1px rgba(0, 0, 0, .5)'}}>Austin, TX.</span></p>
 					<p>I enjoy creating things with code!</p>
 				</figcaption>
