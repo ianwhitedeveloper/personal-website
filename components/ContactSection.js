a00c11eb8bc9d1b3b892493b61357a39d1833048
Ian White <ian.white@t-3.com>
2015-09-05 13:04:35 -0500

    add stuff to contact/about section

--- components/ContactSection.js
+++ components/ContactSection.js
@@ -4,12 +4,12 @@ import mui from 'material-ui';
 const FlatButton = mui.FlatButton;
 const FontIcon = mui.FontIcon;
 
+const resumeLink = 'https://docs.google.com/document/d/1RO41Xdr95pISO0-N9HFoWVIshQYLj_xS1w-uN7IpR5g/edit?usp=sharing';
+const githubLink = 'https://github.com/Iawhite76';
+const emailAddy = 'mailto:ianwhite@utexas.edu';
+const linkedin = 'https://www.linkedin.com/in/iawhite76';
+
 const styles = {
-	container: {
-		width: '100%',
-		display: 'flex',
-		flexFlow: 'row nowrap'
-	},
 	buttonIcon: {
 		position:'relative',
 		fontSize:'24px',
@@ -19,6 +19,18 @@ const styles = {
 		verticalAlign:'middle',
 		float:'left',
 		paddingLeft:'12px',
+		lineHeight:'36px',
+		color:'#00bcd4'
+	},
+	buttonIconFA: {
+		position:'relative',
+		fontSize:'24px',
+		display:'inline-block',
+		transition:'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+		height:'100%',
+		verticalAlign:'middle',
+		float:'left',
+		paddingLeft:'15px',
 		lineHeight:'36px',
 		color:'#00bcd4'
 	},
@@ -32,12 +44,32 @@ export default class ContactSection {
 		return (
 			<section>
 				<div style={{textAlign: 'center'}}>
-					<h1>Contact</h1>
+					<h1>Contact/About Me</h1>
 				</div>
 
-				<div style={styles.container}>
-				  <FlatButton style={styles.button} linkButton={true} href='mailto:ianwhite@utexas.edu?Subject=Oh%hai' label='Email Me - IanWhite@Utexas.edu'>
+				<div className='col-xs-12 col-sm-12 col-md-9'>
+					<p>
+						Some time in 2013 a good friend of mine introduced me to web development and I was immediately hooked.
+					</p>
+					<p>In early 2014 I quit my job and attended <span>MakerSquare</span>, a coding bootcamp that helped launch me into a new career as a full stack web developer.</p>
+					<p>I really enjoy solving problem solving and creating sites my clients can love! Also, I love to be part of a collaborative team and look forward to a lifetime of learning and opportunites.</p>
+				</div>
+
+				<div className='col-xs-12 col-sm-12 col-md-3'>
+				  <FlatButton style={styles.button} linkButton={true} target='_self' href={emailAddy} label='Email'>
 				    <FontIcon style={styles.buttonIcon} className='material-icons' hoverColor='#ddd'>email</FontIcon>
+				  </FlatButton>
+
+				  <FlatButton style={styles.button} linkButton={true} target='_blank' href={resumeLink} label='Resume'>
+				    <FontIcon style={styles.buttonIcon} className='material-icons' hoverColor='#ddd'>description</FontIcon>
+				  </FlatButton>
+
+				  <FlatButton style={styles.button} linkButton={true} target='_blank' href={githubLink} label='Github'>
+				    <FontIcon style={styles.buttonIconFA} className='fa fa-github' hoverColor='#ddd'></FontIcon>
+				  </FlatButton>
+
+				  <FlatButton style={styles.button} linkButton={true} target='_blank' href={linkedin} label='Linkedin'>
+				    <FontIcon style={styles.buttonIconFA} className='fa fa-linkedin-square' hoverColor='#ddd'></FontIcon>
 				  </FlatButton>
 				</div>
 			</section>
