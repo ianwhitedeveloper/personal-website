e0cf0b9680885d54e02bc5a629f98bde6f27d43b
Ian White <ian.white@t-3.com>
2015-09-05 00:06:13 -0500

    remove react transition group because it doesn't play nice with safari :( Add babel react prefixer to add css prefixes to react inline styles

--- components/SkillsSection.js
+++ components/SkillsSection.js
@@ -5,16 +5,10 @@ const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 export default class SkillsSection {
 	render() {
-		const { skills, filterSkills, actions } = this.props;
 
-		let filteredSkills = skills.map((skill, i) => {
-					if (skill.show) {
-						return <div className='skill' style={{padding: '10px 10px', margin: '10px auto'}} key={i}>{skill.title}</div>
-					}
-				});
-
+		const { skills, actions } = this.props;
+		
 		return (
-
 			<section>
 				<div style={{textAlign: 'center'}}>
 					<h1>Skills</h1>
@@ -22,10 +16,14 @@ export default class SkillsSection {
 
 				<div style={{backgroundColor: '#fff', padding: '0 15px', boxShadow: '0 14px 45px rgba(0, 0, 0, 0.25), 0 10px 18px rgba(0, 0, 0, 0.22)'}}>
 					<TextInput {...actions} placeholder='Search...' />
-					<div>
-						<ReactCSSTransitionGroup style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', textAlign: 'center'}} transitionName="example">
-		          {filteredSkills}
-		        </ReactCSSTransitionGroup>
+					<div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', textAlign: 'center'}}>
+						{
+							skills.map((skill, i) => {
+								if (skill.show) {
+									return <div className='skill' style={{padding: '10px 10px', margin: '10px auto'}} key={i}>{skill.title}</div>
+								}
+							})
+						}
 					</div>
 				</div>
 			</section>
