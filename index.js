12d41094b0c45d82a06337211a6c600a72a49ff9
Ian White <ian.white@t-3.com>
2015-09-06 16:40:50 -0500

    clear window hash on initial startup

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ import React from 'react';
 import Root from './containers/Root';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-//Needed for onTouchTap
-//Can go away when react 1.0 release
-//Check this repo:
-//https://github.com/zilverline/react-tap-event-plugin
+//////////////////////////////////////////////////////////////////////////
+// Clear hash on initial start in case one is left over from navigation //
+//////////////////////////////////////////////////////////////////////////
+history.pushState('', document.title, window.location.pathname);
+
+/////////////////////////////////////////////////////////
+//Needed for onTouchTap                                //
+//Can go away when react 1.0 release                   //
+//Check this repo:                                     //
+//https://github.com/zilverline/react-tap-event-plugin //
+/////////////////////////////////////////////////////////
 injectTapEventPlugin();
 React.render(
   <Root />,
