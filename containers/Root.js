9057bb5403e5151f4207b8a5922269d420e1bdb2
Ian White <ian.white@t-3.com>
2015-09-10 07:58:11 -0500

    update copy

--- containers/Root.js
+++ containers/Root.js
@@ -12,28 +12,28 @@ let temporaryInitialState = {
   projects: [
     {
       title: 'UPS Houston',
-      description: 'As the sole developer for this project, I worked with art, account and creative departments to bring this campaign to life. Grunt, Vagrant, Yeoman, Bootstrap, Javascript and jQuery are used on the front end with integration to backend database and authentication technologies via Silverpop and Linkedin REST APIs. Fully responsive. Completed project on time and to full client satisfaction. Upon completion of this project, I created a Yeoman generator for the UPS development team so that future projects of this scope could be more quickly and efficiently developed. Code can be found here: https://github.com/Iawhite76/generator-webapp-vagrant',
+      description: 'Sole developer for this project. Worked collaboratively  with art, account and creative departments to bring this campaign to life. Utilized Grunt, Vagrant, Yeoman, Bootstrap, Javascript and jQuery on the front end. Integrated backend database and authentication technologies including Silverpop and Linkedin REST APIs. Fully responsive and browser compliant to IE8. Completed project on time and to full client satisfaction. Upon completion of this project, I created a Yeoman generator for the UPS development team so that future projects of this scope could be more quickly and efficiently developed. Code can be found here: https://github.com/Iawhite76/generator-webapp-vagrant',
       url: 'https://houston.ups.com',
       imgSrc: '../assets/images/houston.jpg',
       likes: 0
     },
     {
       title: 'UPS Peak',
-      description: 'Responsible for development of a majority of the front end styling based off Photoshop comps delivered by creative department. I volunteered to join the team responsible for this site to help finish project during busy holiday season. I was able to quickly get up to speed and contribute helping to complete the project by the deadline.',
+      description: 'Developed of a majority of the front end styling based off Photoshop comps delivered by creative department. Fully responsive and browser compliant to IE8. Volunteered to join the team responsible for this site to help finish project during busy holiday season. I was able to quickly get up to speed and contribute helping to complete the project by the deadline.',
       url: 'https://wishesdelivered.ups.com/#all-wishes',
       imgSrc: '../assets/images/peak.jpg',
       likes: 0
     },
     {
       title: 'UPS mDot',
-      description: 'Principal front end developer/maintainer of UPS mdot site - a website utilized by many thousands of users each month. Responsible for turning creative specs numbering hundreds of pages into clean and semantic front end code to be handed off to UPS back end engineers. My work has been routinely delivered on or before project deadlines.',
+      description: 'Principal front end developer/maintainer of UPS mdot site - a website utilized by many thousands of users each month. Developed and maintained clean and semantic front end code based on creative specs numbering hundreds of pages to be handed off to UPS back end engineers. Work has been routinely delivered on or before project deadlines.',
       url: 'https://m.ups.com/mDot/',
       imgSrc: '../assets/images/mdot.png',
       likes: 0
     },
     {
       title: 'UPS Healthcare',
-      description: 'Responsible for many of the Javascript and CSS animations as well as other front end functionality seen throughout the pages on this site.',
+      description: 'Another project where I was able to jump in and assist the team so the project could be finished by a tight deadline. Fully responsive and browser compliant to IE8. Developed many of the Javascript and CSS animations as well as other front end functionality seen throughout the pages on this site.',
       url: 'https://healthcare.ups.com/finding-efficiency-through-healthcare-supply-chain-management/',
       imgSrc: '../assets/images/healthcare.jpg',
       likes: 0
